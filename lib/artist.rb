314e92bbb7ba01c7949ba87c0eba3879cd230d93
Ninibeth Teixeira <ninibethreina@gmail.com>
2020-05-05 21:59:13 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,27 @@
+class Artist 
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def songs 
+    Song.all.select do |song|
+      song.artist == self 
+    end 
+  end 
+  
+  def new_song(name, genre)
+    new_song = Song.new(name,genre)
+    new_song.artist == self 
+  end 
+  
+end 
